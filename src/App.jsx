556e3cb68184d6dbb8ada618c6dd5bc465d7a25e
oladapo <oladapoelijah49@gmail.com>
2025-01-29 04:49:05 +0100

    advice me

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {useState, useEffect} from 'react'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [advice, setAdvice] = useState("")
+
+    const adviceMe = async () => {
+        const res = await fetch('https://api.adviceslip.com/advice')
+        const data = await res.json();
+        setAdvice(data.slip.advice);
+    }
+
+    useEffect(() => {
+        adviceMe()
+    }, [advice])
+
+    return (
+        <>
+            <h2>Advice ME</h2>
+            <div>
+                <p>{advice}</p>
+                <button onClick={adviceMe}>Click here</button>
+            </div>
+        </>
+    )
+}
+
+export default App
